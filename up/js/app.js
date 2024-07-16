85c07efc248da7eaeffd07aef0153b218b4b2cea
davidchaves <david92nz@gmail.com>
2024-07-16 14:49:01 -0300

    first commit

---
+++ up/js/app.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector(".header__nav--links");
+const burger = document.querySelector(".header__nav--hamburger");
+const burgerFirstLine = document.querySelector(".line-1");
+const burgerSecondLine = document.querySelector(".line-2");
+
+burger.addEventListener("click", () => {
+	burgerFirstLine.classList.toggle("line-1-toggled");
+	burgerSecondLine.classList.toggle("line-2-toggled");
+
+	nav.classList.toggle("display-nav");
+});
